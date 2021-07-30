e6c484d865f9026a708bbdafeeab091cd69559cb
brayanl <brayan.loucher@outlook.com>
2021-07-30 20:10:36 +0200

    Ajout des vaisseaux ennemies ainsi que du tireur

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.grille');
 let allDivs;
 let alienInvaders = [];
+let shooterPosition = 229;
 
 function creationGrilleEtAliens(){
 
@@ -25,6 +26,24 @@ function creationGrilleEtAliens(){
         }
     }
 
+    for(i = 1; i < 53; i++){
+        if(i === 13){
+            i = 21;
+            alienInvaders.push(i);
+        } else if(i === 33) {
+            i = 41;
+            alienInvaders.push(i);
+        } else {
+            alienInvaders.push(i);
+        }
+    }
+
+    allDivs = document.querySelectorAll('.grille div');
+    alienInvaders.forEach(invader => {
+        allDivs[invader].classList.add('aliens');
+    })
+
+    allDivs[shooterPosition].classList.add('tireur');
 }
 
 creationGrilleEtAliens();
